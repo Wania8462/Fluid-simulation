bcf99e60e7b9aa305c391ac1eec77e72596b1892
Wania8462 <65669738+Wania8462@users.noreply.github.com>
2025-12-22 10:32:04 +0000

    Density at a point

--- Assets/Scripts/Implementation/Simulation.cs
+++ Assets/Scripts/Implementation/Simulation.cs
@@ -10,6 +10,7 @@ public class Simulation : MonoBehaviour
     [SerializeField] private float gravity;
     [SerializeField] private float collisionDamp;
     [SerializeField] private float mass;
+    [SerializeField] private float smoothingRadius;
 
     private CreateCubeMesh render;
     private SpawnParticles spawn;
@@ -45,35 +46,58 @@ public class Simulation : MonoBehaviour
     }
 
     [BurstCompile]
+    private float Density(float3 pos)
+    {
+        float density = 0;
+
+        for (int i = 0; i < points.Length; i++)
+            density += mass * SmoothingKernelPoly6(Distance(pos, points[i]));
+        
+        return density;
+    }
+
+    [BurstCompile]
     private void ResolveCollisions()
     {
         for (int i = 0; i < points.Length; i++)
         {
-            if(Mathf.Abs(points[i].x) >= realHalfBoundSize.x)
+            if (Mathf.Abs(points[i].x) >= realHalfBoundSize.x)
             {
                 points[i].x = realHalfBoundSize.x * Mathf.Sign(points[i].x);
                 velocities[i].x *= -1 * collisionDamp;
             }
-            
-            else if(Mathf.Abs(points[i].y) >= realHalfBoundSize.y)
+
+            else if (Mathf.Abs(points[i].y) >= realHalfBoundSize.y)
             {
                 points[i].y = realHalfBoundSize.y * Mathf.Sign(points[i].y);
                 velocities[i].y *= -1 * collisionDamp;
             }
-            
-            else if(Mathf.Abs(points[i].z) >= realHalfBoundSize.z)
+
+            else if (Mathf.Abs(points[i].z) >= realHalfBoundSize.z)
             {
                 points[i].z = realHalfBoundSize.z * Mathf.Sign(points[i].z);
                 velocities[i].z *= -1 * collisionDamp;
             }
-            
+
         }
     }
 
     [BurstCompile]
-    private float SmoothingKernelPoly6(float dist, float cutoff)
+    private float SmoothingKernelPoly6(float dist)
     {
-        float numerator = 315 * Mathf.Pow(cutoff * cutoff - dist * dist, 3);
-        return numerator / 64 * Mathf.PI * Mathf.Pow(cutoff, 9);
+        if (dist > smoothingRadius)
+            return 0;
+
+        float numerator = 315 * Mathf.Pow(smoothingRadius * smoothingRadius - dist * dist, 3);
+        return numerator / 64 * Mathf.PI * Mathf.Pow(smoothingRadius, 9);
+    }
+
+    [BurstCompile]
+    private float Distance(float3 pos1, float3 pos2)
+    {
+        float distX = Mathf.Pow(Mathf.Abs(pos1.x - pos2.x), 2);
+        float distY = Mathf.Pow(Mathf.Abs(pos1.y - pos2.y), 2);
+        float distZ = Mathf.Pow(Mathf.Abs(pos1.z - pos2.z), 2);
+        return Mathf.Sqrt(distX + distY + distZ);
     }
 }
